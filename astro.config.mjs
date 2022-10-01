7e926fa02c4188e40dbba39117e4ea315c8fe81e
Ryan Trimble <mrtrimble2@gmail.com>
2022-10-01 12:07:16 -0400

    init commit

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+
+import netlify from '@astrojs/netlify/edge-functions';
+
+// https://astro.build/config
+export default defineConfig({
+  output: "server",
+  adapter: netlify()
+});
