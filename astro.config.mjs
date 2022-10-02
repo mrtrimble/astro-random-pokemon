9c5038ca28fba094f4d19bf636690cb832ce0303
Ryan Trimble <mrtrimble2@gmail.com>
2022-10-02 18:23:49 -0400

    compress, update meta info, add robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from 'astro/config';
-
+import robotsTxt from 'astro-robots-txt';
 import netlify from '@astrojs/netlify/edge-functions';
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  site: 'https://astro-pokemon.netlify.app/',
+  experimental: {
+    integrations: true
+  },
+  integrations: [robotsTxt(), compress()],
+  output: 'server',
   adapter: netlify()
 });
